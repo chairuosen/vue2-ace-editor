40fcfbbaea80a761233e090324c23d27459d2654
Lawrence Cherone <lcherone@users.noreply.github.com>
2018-02-18 01:08:17 +0000

    Fix linting, fixed to work with electron-vue
    Removed all `;`, switched out double quotes for single and spacing linting errors, changed init emmet to `window.emmet = require('emmet/emmet')`.
    
    Now works with electron vue.

--- index.js
+++ index.js
@@ -1,70 +1,66 @@
-var ace = require('brace');
+var ace = require('brace')
 
-require(['emmet/emmet'],function (data) {
-    window.emmet = data.emmet;
-});
+window.emmet = require('emmet/emmet')
 
 module.exports = {
-    template:"<div :style=\"{height: height ? px(height) : '100%',width: width ? px(width) : '100%'}\"></div>",
-    props:{
-        value:{
-            type:String,
-            required:true
-        },
-        lang:String,
-        theme:String,
-        height:true,
-        width:true
+  template: `<div style="position: relative" :style="{ height: height ? px(height) : '100%', width: width ? px(width) : '100%'}"></div>`,
+  props: {
+    value: {
+      type: String,
+      required: true
     },
-    data: function () {
-        return {
-            editor:null,
-            contentBackup:""
-        }
-    },
-    methods: {
-        px:function (n) {
-            if( /^\d*$/.test(n) ){
-                return n+"px";
-            }
-            return n;
-        }
-    },
-    watch:{
-        value:function (val) {
-            if(this.contentBackup !== val)
-                this.editor.setValue(val,1);
-        },
-        theme:function (newTheme) {
-            this.editor.setTheme('ace/theme/'+newTheme);
-        },
-        lang:function (newLang) {
-            this.editor.getSession().setMode('ace/mode/'+newLang);
-        }
-    },
-    mounted: function () {
-        var vm = this;
-        var lang = this.lang||'text';
-        var theme = this.theme||'chrome';
-
-        require('brace/ext/emmet');
-
-        var editor = vm.editor = ace.edit(this.$el);
-
-        this.$emit('init',editor);
-        
-        editor.$blockScrolling = Infinity;
-        editor.setOption("enableEmmet", true);
-        editor.getSession().setMode('ace/mode/'+lang);
-        editor.setTheme('ace/theme/'+theme);
-        editor.setValue(this.value,1);
-
-        editor.on('change',function () {
-            var content = editor.getValue();
-            vm.$emit('input',content);
-            vm.contentBackup = content;
-        });
-
-
+    lang: String,
+    theme: String,
+    height: true,
+    width: true
+  },
+  data: function () {
+    return {
+      editor: null,
+      contentBackup: ''
     }
+  },
+  methods: {
+    px (n) {
+      if (/^\d*$/.test(n)) {
+        return n + 'px'
+      }
+      return n
+    }
+  },
+  watch: {
+    value: function (val) {
+      if (this.contentBackup !== val) {
+        this.editor.setValue(val, 1)
+      }
+    },
+    theme: function (newTheme) {
+      this.editor.setTheme('ace/theme/' + newTheme)
+    },
+    lang: function (newLang) {
+      this.editor.getSession().setMode('ace/mode/' + newLang)
+    }
+  },
+  mounted: function () {
+    var lang = this.lang || 'text'
+    var theme = this.theme || 'chrome'
+
+    require('brace/ext/emmet')
+
+    this.editor = ace.edit(this.$el)
+
+    this.$emit('init', this.editor)
+
+    this.editor.$blockScrolling = Infinity
+    this.editor.setOption('enableEmmet', true)
+    this.editor.getSession().setMode('ace/mode/' + lang)
+    this.editor.setTheme('ace/theme/' + theme)
+    this.editor.setValue(this.value, 1)
+
+    this.editor.on('change', () => {
+      var content = this.editor.getValue()
+      this.$emit('input', content)
+      this.contentBackup = content
+    })
+  }
 }
